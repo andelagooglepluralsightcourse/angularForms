7a160059b940ca4e02d6d98994a0a493ed58b8c8
Kabali Michael <fitzerkisakyamukama03@gmail.com>
2020-07-16 21:58:48 +0300

    dynamicallly loading dropdown values

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { UserSettings } from '../data/user-settings';
 import { NgForm } from '@angular/forms';
+
+import { DataService } from '../data/data.service';
+import { Observable } from 'rxjs/internal/Observable';
+
 @Component({
   selector: 'app-user-settings-form',
   templateUrl: './user-settings-form.component.html',
@@ -18,13 +22,39 @@ export class UserSettingsFormComponent implements OnInit {
   }
 
   userSettings: UserSettings = {...this.originalUserSettings}
-  constructor() { }
+  postError = false;
+  postErrorMessage = '';
+  subscriptionTypes: Observable<string[]>;
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
+    this.subscriptionTypes =  this.dataService.getSubscriptionTypes()
+  }
+
+  onHttpError(errorResponse: any) {
+    console.log('error', errorResponse);
+    this.postError = true;
+    this.postErrorMessage = errorResponse.error.errorMessage;
+
   }
 
   onSubmit(form: NgForm){
     console.log('in onSubmit: ', form.valid );
+    if (form.valid) {
+      this.dataService.postUserSettingForm(this.userSettings).subscribe(
+        result => console.log(result),
+        error => this.onHttpError(error)
+      )
+    } else {
+      this.postError =  true;
+      this.postErrorMessage = "Please fix the above errors"
+    }
+
+  }
+
+  onBlur(field: NgForm ){
+    console.log('in onBlur: ', field.valid)
   }
 
 }
